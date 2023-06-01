32db9c89f4c068b23b067f7ec1da06d6facb6cae
Lorena Rojas <102558556+lrojas4@users.noreply.github.com>
2023-06-01 09:31:25 -0500

    added seed

---
+++ src/main/java/com/example/expensetrackerbackend/seed/DataLoader.java
@@ -0,0 +1,9 @@
+package com.example.expensetrackerbackend.seed;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+
+}
