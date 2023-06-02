ec12fff935eeb679a6f889e2598a59b4ab14e4af
Lorena Rojas <102558556+lrojas4@users.noreply.github.com>
2023-06-02 11:57:30 -0500

    added parseJwt()

--- src/main/java/com/example/expensetrackerbackend/security/JwtRequestFilter.java
+++ src/main/java/com/example/expensetrackerbackend/security/JwtRequestFilter.java
@@ -1,7 +1,10 @@
 package com.example.expensetrackerbackend.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.http.HttpServletRequest;
 import java.util.logging.Logger;
 
 @Component
@@ -14,4 +17,17 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
     @Autowired
     private JWTUtils jwtUtils;
+
+    /**
+     * Removes bearer from authorization
+     * @param request authorization header value
+     * @return jwt token
+     */
+    private String parseJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+        if (StringUtils.hasLength("headerAuth") && headerAuth.startsWith("Bearer")) {
+            return headerAuth.substring(7);
+        }
+        return null;
+    }
 }
