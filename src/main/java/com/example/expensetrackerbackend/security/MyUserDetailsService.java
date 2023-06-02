1be2b0355e0ec4d352a960fa1f3868cca233b842
Lorena Rojas <102558556+lrojas4@users.noreply.github.com>
2023-06-02 12:05:55 -0500

    update docstring

--- src/main/java/com/example/expensetrackerbackend/security/MyUserDetailsService.java
+++ src/main/java/com/example/expensetrackerbackend/security/MyUserDetailsService.java
@@ -21,12 +21,11 @@ public class MyUserDetailsService implements UserDetailsService {
      * Finds user by their email address.
      * @param username email for user.
      * @return The user details with that email.
-     * @throws UsernameNotFoundException When email is not connected to an agent.
+     * @throws UsernameNotFoundException When email is not connected to a user.
      */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userService.findUserByEmailAddress(username);
         return new MyUserDetails(user);
     }
-
 }
