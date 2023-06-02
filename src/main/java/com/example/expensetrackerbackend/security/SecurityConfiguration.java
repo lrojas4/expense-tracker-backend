1f7294e31f32af07c028b1b080d9a8a1ee4eae04
Lorena Rojas <102558556+lrojas4@users.noreply.github.com>
2023-06-02 12:17:29 -0500

    added authJwtRequestFilter() and passwordEncoder()

--- src/main/java/com/example/expensetrackerbackend/security/SecurityConfiguration.java
+++ src/main/java/com/example/expensetrackerbackend/security/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package com.example.expensetrackerbackend.security;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -12,6 +14,16 @@ public class SecurityConfiguration {
     @Autowired
     public void setMyUserDetailsService(MyUserDetailsService myUserDetailsService) {
         this.myUserDetailsService = myUserDetailsService;
+    }
+
+    @Bean
+    public JwtRequestFilter authJwtRequestFilter() {
+        return new JwtRequestFilter();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
