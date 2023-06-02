9df8220117cf5e6269cde10ddb11cdec99a14af3
Lorena Rojas <102558556+lrojas4@users.noreply.github.com>
2023-06-02 12:16:12 -0500

    added SecurityConfiguration class

---
+++ src/main/java/com/example/expensetrackerbackend/security/SecurityConfiguration.java
@@ -0,0 +1,17 @@
+package com.example.expensetrackerbackend.security;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration {
+
+    private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    public void setMyUserDetailsService(MyUserDetailsService myUserDetailsService) {
+        this.myUserDetailsService = myUserDetailsService;
+    }
+
+}
