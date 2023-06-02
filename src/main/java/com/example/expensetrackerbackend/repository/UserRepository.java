0ad835f5ed612c076500c9c471c34d6c5baff94a
Lorena Rojas <102558556+lrojas4@users.noreply.github.com>
2023-06-02 15:13:16 -0500

    added login and register

--- src/main/java/com/example/expensetrackerbackend/repository/UserRepository.java
+++ src/main/java/com/example/expensetrackerbackend/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.example.expensetrackerbackend.repository;
-
 import com.example.expensetrackerbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    //Login
+    boolean existsByEmail(String email);
+
+    //Register
+    User findUserByEmail(String email);
 }
