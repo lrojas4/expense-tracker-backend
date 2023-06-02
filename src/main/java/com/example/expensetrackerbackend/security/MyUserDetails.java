1ff8b3a9dc32e88f0b69f0af3cbcc53597e0f12c
Lorena Rojas <102558556+lrojas4@users.noreply.github.com>
2023-06-02 10:56:56 -0500

    added getAuthorities()

--- src/main/java/com/example/expensetrackerbackend/security/MyUserDetails.java
+++ src/main/java/com/example/expensetrackerbackend/security/MyUserDetails.java
@@ -1,13 +1,22 @@
 package com.example.expensetrackerbackend.security;
 
 import com.example.expensetrackerbackend.model.User;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
 
 public class MyUserDetails implements UserDetails {
     private final User user;
 
     public MyUserDetails(User user) {
         this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new HashSet<>();
     }
 
 
