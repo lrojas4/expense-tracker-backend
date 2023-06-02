16ab87760529e38fb44fcf1f6adcc60496e0192b
Lorena Rojas <102558556+lrojas4@users.noreply.github.com>
2023-06-02 11:37:44 -0500

    added setUserRepository()

--- src/main/java/com/example/expensetrackerbackend/service/UserService.java
+++ src/main/java/com/example/expensetrackerbackend/service/UserService.java
@@ -1,9 +1,54 @@
 package com.example.expensetrackerbackend.service;
 
+import com.example.expensetrackerbackend.exception.InformationExistException;
+import com.example.expensetrackerbackend.model.User;
+import com.example.expensetrackerbackend.repository.UserRepository;
+import com.example.expensetrackerbackend.security.JWTUtils;
+import com.example.expensetrackerbackend.security.MyUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.security.authentication.AuthenticationManager;
 
 @Service
 public class UserService {
+    private UserRepository userRepository;
+    private AuthenticationManager authenticationManager;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private JWTUtils jwtUtils;
+
+    private MyUserDetails myUserDetails;
+
+    public UserService(UserRepository userRepository, AuthenticationManager authenticationManager,
+                       PasswordEncoder passwordEncoder, JWTUtils jwtUtils, MyUserDetails myUserDetails) {
+        this.userRepository = userRepository;
+        this.authenticationManager = authenticationManager;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtils = jwtUtils;
+        this.myUserDetails = myUserDetails;
+    }
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /**
+     * Registers a new user if email is not currently being used.
+     * @param userObject User's information to be saved with their profile.
+     * @return A new user.
+     * @throws InformationExistException if email is already in use with a different agent.
+     */
+    public User registerAgent(User userObject) {
+        if(!userRepository.existsByEmail(userObject.getEmail())) {
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
+            return userRepository.save(userObject);
+        } else {
+            throw new InformationExistException("An agent with email " + userObject.getEmail() + " already exists.");
+        }
+    }
 
 
 
