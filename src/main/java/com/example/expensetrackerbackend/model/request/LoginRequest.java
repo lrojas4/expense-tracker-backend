f35f7d71ea27e44168ddfbbc52d265a63a421e74
Lorena Rojas <102558556+lrojas4@users.noreply.github.com>
2023-06-02 11:45:41 -0500

    added LoginRequest class

---
+++ src/main/java/com/example/expensetrackerbackend/model/request/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.example.expensetrackerbackend.model.request;
+
+public class LoginRequest {
+
+    private String email;
+    private String password;
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+}
