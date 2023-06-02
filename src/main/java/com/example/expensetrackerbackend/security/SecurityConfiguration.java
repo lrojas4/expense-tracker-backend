f121006aa7f9e88b4e735ef5b199827701ed3abd
Lorena Rojas <102558556+lrojas4@users.noreply.github.com>
2023-06-02 12:21:27 -0500

    added authenticationManager()

--- src/main/java/com/example/expensetrackerbackend/security/SecurityConfiguration.java
+++ src/main/java/com/example/expensetrackerbackend/security/SecurityConfiguration.java
@@ -3,6 +3,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -52,6 +54,17 @@ public class SecurityConfiguration {
                 .and().csrf().disable();
         http.addFilterBefore(authJwtRequestFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    /**
+     * Handles authentication requests.
+     * @param authConfig Is the value that is being authenticated.
+     * @return Parameter with authentication API
+     * @throws Exception Any exception that may come.
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 
 }
