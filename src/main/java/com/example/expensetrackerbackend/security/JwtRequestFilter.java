c2ba12ceccd48087f30517251da394c5fed14c38
Lorena Rojas <102558556+lrojas4@users.noreply.github.com>
2023-06-02 11:59:23 -0500

    added doFilterInternal()

--- src/main/java/com/example/expensetrackerbackend/security/JwtRequestFilter.java
+++ src/main/java/com/example/expensetrackerbackend/security/JwtRequestFilter.java
@@ -1,10 +1,18 @@
 package com.example.expensetrackerbackend.security;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 @Component
@@ -29,5 +37,37 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             return headerAuth.substring(7);
         }
         return null;
+    }
+
+    /**
+     * Authenticates the user based on a JSON Web Token (JWT) that is provided in the request header
+     * @param request the HTTP request object
+     * @param response the HTTP response object
+     * @param filterChain the filter chain to execute after authentication
+     * @throws ServletException if there is an error in the servlet
+     * @throws IOException  if there is an I/O error
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            String jwt = parseJwt(request);
+            // check if the jwt key is valid and not null
+            if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+                // if valid get user email from the key
+                String username = jwtUtils.getUserNameFromJwtToken(jwt);
+                // load user details from the key
+                UserDetails userDetails = this.myUserDetailsService.loadUserByUsername(username);
+                // set username and password authentication token from user user details
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                // build request and get security content
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (Exception e) {
+            logger.info("Cannot set user authentication");
+        }
+        filterChain.doFilter(request, response);
     }
 }
