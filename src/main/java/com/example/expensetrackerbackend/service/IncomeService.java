f0de38f5b05a1c17679655178ce7d61de1961c0c
Lorena Rojas <102558556+lrojas4@users.noreply.github.com>
2023-06-03 16:02:47 -0500

    added getIncome()

--- src/main/java/com/example/expensetrackerbackend/service/IncomeService.java
+++ src/main/java/com/example/expensetrackerbackend/service/IncomeService.java
@@ -1,5 +1,4 @@
 package com.example.expensetrackerbackend.service;
-import com.example.expensetrackerbackend.model.Expense;
 import com.example.expensetrackerbackend.model.Income;
 import com.example.expensetrackerbackend.repository.IncomeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +23,16 @@ public class IncomeService {
     public List<Income> getIncomes() {
         return incomeRepository.findAll();
     }
+
+    /**
+     * Gets income by income id
+     * @param incomeId we are searching for
+     * @return income based on id
+     */
+    public Income getIncome(Long incomeId) {
+        return incomeRepository.findById(incomeId).orElse(null);
+    }
+
 
     /**
      * Filters a list of incomes by user id
