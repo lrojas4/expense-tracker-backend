f4e986676d553bf70b66c44ac29eaaa14420c8c5
Lorena Rojas <102558556+lrojas4@users.noreply.github.com>
2023-06-03 11:47:57 -0500

    added getIncomes() and getIncomesByUserId()

--- src/main/java/com/example/expensetrackerbackend/service/IncomeService.java
+++ src/main/java/com/example/expensetrackerbackend/service/IncomeService.java
@@ -1,9 +1,11 @@
 package com.example.expensetrackerbackend.service;
-
-import com.example.expensetrackerbackend.repository.ExpenseRepository;
+import com.example.expensetrackerbackend.model.Expense;
+import com.example.expensetrackerbackend.model.Income;
 import com.example.expensetrackerbackend.repository.IncomeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class IncomeService {
@@ -13,5 +15,23 @@ public class IncomeService {
     @Autowired
     public void setIncomeRepository(IncomeRepository incomeRepository) {
         this.incomeRepository = incomeRepository;
+    }
+
+    /**
+     * Gets a list of incomes
+     * @return a list of incomes
+     */
+    public List<Income> getIncomes() {
+        return incomeRepository.findAll();
+    }
+
+    /**
+     * Filters a list of incomes by user id
+     * @param userId we are searching for
+     * @return a list of incomes based on user id
+     */
+    public List<Income> getIncomesByUserId(Long userId) {
+        return incomeRepository.findAll().stream().filter(income -> income.getUser().getId() == userId)
+                .collect(Collectors.toList());
     }
 }
